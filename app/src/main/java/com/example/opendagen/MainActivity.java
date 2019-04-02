872d6031eaacc8304295675ba65e008c1efb86f3
Jeffrey <jeffrey.post@hotmail.com>
2019-04-02 16:35:22 +0200

    krijg toch allemaal de klere

--- app/src/main/java/com/example/opendagen/MainActivity.java
+++ app/src/main/java/com/example/opendagen/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.opendagen;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,12 +18,28 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonAgenda;
     private Button buttonSocialMedia;
     private Button buttonShare;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
 
         //knop om naar Open Dagen te gaan.
         buttonOpenDagen = (Button) findViewById(R.id.buttonOpenDagen);
@@ -30,14 +49,7 @@ public class MainActivity extends AppCompatActivity {
                 openOpenDagen();
             }
         });
-        //knop om naar Agenda te gaan.
-        buttonAgenda = (Button) findViewById(R.id.buttonAgenda);
-        buttonAgenda.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openAgenda();
-            }
-        });
+
         //knop om naar Locatie te gaan.
         buttonLocatie = (Button) findViewById(R.id.buttonLocatie);
         buttonLocatie.setOnClickListener(new View.OnClickListener() {
@@ -67,9 +79,10 @@ public class MainActivity extends AppCompatActivity {
         Intent intent1 = new Intent(this, Activity2.class);
         startActivity(intent1);
     }
-    public void openAgenda() {
-        Intent intent2 = new Intent(this, Activity5.class);
-        startActivity(intent2);
+    public void OpenCalender(View view){
+        Intent OpenCalendarApp = getPackageManager().getLaunchIntentForPackage("com.google.android.calendar");
+        startActivity(OpenCalendarApp);
+
     }
     public void openLocatie() {
         Intent intent4 = new Intent(this, Activity4.class);
@@ -82,5 +95,14 @@ public class MainActivity extends AppCompatActivity {
     public void openShare() {
         Intent intent6 = new Intent(this, Activity7.class);
         startActivity(intent6);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
