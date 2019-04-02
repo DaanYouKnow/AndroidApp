07505a071e7a48217e2c6ed9aba1ddd0e58269f7
DaanYouKnow <0975946@hr.nl>
2019-04-02 15:16:02 +0200

    Social Media Update

--- app/src/main/java/com/example/opendagen/Activity6.java
+++ app/src/main/java/com/example/opendagen/Activity6.java
@@ -1,14 +1,74 @@
 package com.example.opendagen;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 import com.example.opendagen.R;
+
+
 public class Activity6 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_6);
+
+        Button ButtonFacebook = (Button)findViewById(R.id.Facebook);
+        ButtonFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Facebook = "https://www.facebook.com/hogeschoolrotterdam/";
+                Uri webadress = Uri.parse(Facebook);
+
+                Intent gotoFacebook = new Intent(Intent.ACTION_VIEW, webadress);
+                if (gotoFacebook.resolveActivity(getPackageManager()) != null){
+                    startActivity(gotoFacebook);
+                }
+            }
+        });
+        Button ButtonInstagram = (Button)findViewById(R.id.Instagram);
+        ButtonInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Insta = "https://www.instagram.com/hogeschoolrotterdam/";
+                Uri webadress = Uri.parse(Insta);
+
+                Intent gotoInstagram = new Intent(Intent.ACTION_VIEW, webadress);
+                if (gotoInstagram.resolveActivity(getPackageManager()) != null){
+                    startActivity(gotoInstagram);
+                }
+            }
+        });
+
+        Button ButtonTwitter = (Button)findViewById(R.id.Twitter);
+        ButtonTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Twitter = "https://twitter.com/hsrotterdam";
+                Uri webadress = Uri.parse(Twitter);
+
+                Intent gotoTwitter = new Intent(Intent.ACTION_VIEW, webadress);
+                if (gotoTwitter.resolveActivity(getPackageManager()) != null){
+                    startActivity(gotoTwitter);
+                }
+            }
+        });
+        Button ButtonWebsite = (Button)findViewById(R.id.Website);
+        ButtonWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Website = "https://www.hogeschoolrotterdam.nl/";
+                Uri webadress = Uri.parse(Website);
+
+                Intent gotoWebsite = new Intent(Intent.ACTION_VIEW, webadress);
+                if (gotoWebsite.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoWebsite);
+                }
+            }
+        });
+
     }
 }
