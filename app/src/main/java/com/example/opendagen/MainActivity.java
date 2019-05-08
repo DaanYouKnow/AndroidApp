78ce3cabf00319bde790bc5ce12c3902317b9e49
DaanYouKnow <0975946@hr.nl>
2019-05-08 12:38:29 +0200

    #asdasdyolo toegevoegd.

--- app/src/main/java/com/example/opendagen/MainActivity.java
+++ app/src/main/java/com/example/opendagen/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private Button buttonSocialMedia;
     private Button buttonShare;
     private DrawerLayout mDrawerLayout;
-    private Button buttonLogin; //yoloxd
+    private Button buttonLogin; //yoloxdyotkeyoy
     private ActionBarDrawerToggle mToggle;
 
 
