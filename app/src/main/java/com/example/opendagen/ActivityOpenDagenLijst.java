4447aa597e0b5d1cfcc8196e47eabfb44cdb5459
amynowe <43269323+amynowe@users.noreply.github.com>
2019-05-19 17:14:32 +0200

    Dark mode --> HRO mode doet ie nu ook op de andere schermen, hij deed t eerst alleen op de mainactivity

--- app/src/main/java/com/example/opendagen/ActivityOpenDagenLijst.java
+++ app/src/main/java/com/example/opendagen/ActivityOpenDagenLijst.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -16,6 +17,11 @@ public class ActivityOpenDagenLijst extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.HROTheme);
+        }
+        else setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.opendagenlijstactivity);
 
