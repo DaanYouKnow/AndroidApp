f3f2335325670f966e96a5cab423d70e1dd4b880
Jurrilicious <jurriaanvanderzwaag@hotmail.com>
2019-05-28 14:19:25 +0200

    Agenda event aanmaken

--- app/src/main/java/com/example/opendagen/ActivityOpenDagenInfo.java
+++ app/src/main/java/com/example/opendagen/ActivityOpenDagenInfo.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.text.method.LinkMovementMethod;
@@ -12,6 +13,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.opendagen.R;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class ActivityOpenDagenInfo extends AppCompatActivity {
 
     @Override
@@ -62,6 +67,77 @@ public class ActivityOpenDagenInfo extends AppCompatActivity {
                 startActivity(Intent.createChooser(myIntent, "Share your excitement!"));
             }
         });
+        Button buttonAgenda = (Button) findViewById(R.id.buttonAgenda);
+        buttonAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                Intent intent = new Intent(Intent.ACTION_EDIT);
+                intent.setType("vnd.android.cursor.item/event");
+                String Sday =  getIntent().getStringExtra("Datum").substring(2,4);
+                String Smon =  getIntent().getStringExtra("Datum");
+                Sday = Sday.replace(" ","");
+                int day = Integer.parseInt(Sday);
+                int mon;
+                if(Smon.toLowerCase().contains("januari")){
+                    mon=0;
+                }
+                else if(Smon.toLowerCase().contains("februari")){
+                    mon=1;
+                }
+                else if(Smon.toLowerCase().contains("maart")){
+                    mon=2;
+                }
+                else if(Smon.toLowerCase().contains("april")){
+                    mon=3;
+                }
+                else if(Smon.toLowerCase().contains("mei")){
+                    mon=4;
+                }
+                else if(Smon.toLowerCase().contains("juni")){
+                    mon=5;
+                }
+                else if(Smon.toLowerCase().contains("juli")){
+                    mon=6;
+                }
+                else if(Smon.toLowerCase().contains("augustus")){
+                    mon=7;
+                }
+                else if(Smon.toLowerCase().contains("september")){
+                    mon=8;
+                }
+                else if(Smon.toLowerCase().contains("oktober")){
+                    mon=9;
+                }
+                else if(Smon.toLowerCase().contains("november")){
+                    mon=12;
+                }
+                else{
+                    mon=11;
+                }
+                int yer = Calendar.getInstance().get(Calendar.YEAR);
+                if(Smon.toLowerCase().contains("2018")) {
+                yer = 2018;
+                }
+                if(Smon.toLowerCase().contains("2019")) {
+                    yer = 2019;
+                }
+                if(Smon.toLowerCase().contains("2020")) {
+                    yer = 2020;
+                }
+                if(Smon.toLowerCase().contains("2021")) {
+                    yer = 2021;
+                }
+                GregorianCalendar calDate = new GregorianCalendar(yer, mon, day);
+                intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        calDate.getTimeInMillis());
+                intent.putExtra("allDay", true);
+                intent.putExtra("title", "Hogeschool Rotterdam Opendag Informatica");
+                startActivity(intent);
 
+
+
+            }
+        });
     }
 }
