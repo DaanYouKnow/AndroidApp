e508392db064c9609ac4dd499bb3987b61071618
Jeffrey <jeffrey.post@hotmail.com>
2019-05-29 11:14:47 +0200

    Navigation drawer ook nog bij opendageninfo toegevoegd

--- app/src/main/java/com/example/opendagen/ActivityOpenDagenInfo.java
+++ app/src/main/java/com/example/opendagen/ActivityOpenDagenInfo.java
@@ -5,9 +5,13 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,7 +21,9 @@ import com.example.opendagen.R;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class ActivityOpenDagenInfo extends AppCompatActivity {
+public class ActivityOpenDagenInfo extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -139,5 +145,62 @@ public class ActivityOpenDagenInfo extends AppCompatActivity {
 
             }
         });
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nv1);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case (R.id.opendays):
+                Intent intent1 = new Intent(getApplicationContext(), ActivityOpenDagenLijst.class);
+                startActivity(intent1);
+                break;
+            case (R.id.social):
+                Intent intent5 = new Intent(getApplicationContext(), ActivityInfo.class);
+                startActivity(intent5);
+                break;
+            case (R.id.locatiez):
+                Intent intent4 = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent4);
+                break;
+            case (R.id.sharee):
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String ShareSub = "Ik ga naar hogeschool Rotterdam!\n";
+                String ShareBody = "ik ga binnenkort naar opendag informatica bij HogeSchool Rotterdam! \nik heb er nu al zin in!";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, ShareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT, ShareBody);
+                startActivity(Intent.createChooser(myIntent, "Share your excitement!"));
+                break;
+            case (R.id.home):
+                Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent2);
+                break;
+            case (R.id.switch2):
+                Intent intent3 = new Intent(getApplicationContext(), SwitchInDrawer.class);
+                startActivity(intent3);
+                break;
+
+
+        }
+        return true;
     }
 }
+
