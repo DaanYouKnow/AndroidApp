619395617ac9b3a84701f2634b3942410e8557b5
DaanYouKnow <0975946@hr.nl>
2019-06-10 17:12:41 +0200

    plattegrond update2. Info Pagina werkt prima, maar settings niet?

--- app/src/main/java/com/example/opendagen/ActivityPlattegrond.java
+++ app/src/main/java/com/example/opendagen/ActivityPlattegrond.java
@@ -1,13 +1,28 @@
 package com.example.opendagen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ActivityPlattegrond extends AppCompatActivity {
 
+    private ImageView Plattegrond;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plattegrond);
+        Plattegrond = findViewById(R.id.imageViewPlattegrond);
+        Plattegrond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openInfo();
+            }
+        });
+    }
+    public void openInfo() {
+        Intent intent = new Intent(this, ActivityInfo.class);
+        startActivity(intent);
     }
 }
