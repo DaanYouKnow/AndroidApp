09a1b8cc9d47527a6922ac0f3b69da4797f2eb9d
Jeffrey <jeffrey.post@hotmail.com>
2019-06-14 22:30:05 +0200

    Kopjes toegevoegd aan info opendagen

--- app/src/main/java/com/example/opendagen/ActivityOpenDagenInfo.java
+++ app/src/main/java/com/example/opendagen/ActivityOpenDagenInfo.java
@@ -11,14 +11,10 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.support.v7.app.AppCompatDelegate;
-import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.example.opendagen.R;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -80,7 +76,7 @@ public class ActivityOpenDagenInfo extends AppCompatActivity implements Navigati
         Tv2.setText(getIntent().getStringExtra("Info"));
         TextView Tv3 = (TextView) findViewById(R.id.textView4);
         Resources res = getResources();
-        Tv3.setText(res.getString(R.string.kankertext));
+        Tv3.setText(res.getText(R.string.verwachttext));
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
